a96aa2592b52f933074ac9c304f52ca3d7ab60ba
Aditya200410 <adityakumar098k@gmail.com>
2025-10-03 17:26:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,12 +31,14 @@ const app = express();
 // CORS configuration - Allow specific origins for production
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://localhost:5174',
+  
+  'https://admin.waterparkchalo.com',
   'https://waterpark-frontend.vercel.app',
   'https://water-admin-lyart.vercel.app',
   'https://www.waterparkchalo.com',
+  'https://waterparkchalo.com',
  
-  'https://admin.waterparkchalo.com'
+
  
   
 ];
